2fc1eb7492dec63ec3479615d50ef072f6236d69
Abdullah_Al_Mahfuz <mahfuzabdulla1212@gmail.com>
2026-09-19 22:35:19 +0600

    git push

---
+++ 7_list_elements.py
@@ -0,0 +1,6 @@
+names=['alice', 'bob', 'charlie', 'david', 'eve']
+
+for name in names:
+    print(f"Welcome to the channel, {name.title()}")
+    print(f"Nice to meet you, {name.upper()}")
+print('the loop ended')
